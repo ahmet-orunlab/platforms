bbc91964810549690f09b2a5222e0c347f4c5222
ahmet-orunlab <ahmet@orunlab.com>
2026-07-11 16:19:36 +0300

    arranged container internal folder structure for customer

--- platforms/tivac-lab/docker_run.sh
+++ platforms/tivac-lab/docker_run.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 docker rm -f tivac_running_container 2>/dev/null
+docker build -t tivac-lab-env .
 
 docker run -d \
   --name tivac_running_container \
   --privileged \
   -p 2222:22 \
   -v /dev/bus/usb:/dev/bus/usb \
-  -v $(pwd):/workspace \
+  -v $(pwd)/projects:/workspace \
   tivac-lab-env
 
 echo "Tiva C Konteyneri izole SSH portuyla (2222) sunucuda baslatildi."
